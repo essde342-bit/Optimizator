2516c963fe792727bd2e77003a65876796b76201
To4noHe_BadBoy <essde342@gmail.com>
2026-09-21 12:44:42 +0300

    fix: use mixin shadow for pause menu button

--- src/main/java/com/essde/optimizator/mixin/GameMenuScreenMixin.java
+++ src/main/java/com/essde/optimizator/mixin/GameMenuScreenMixin.java
@@ -2,29 +2,38 @@ package com.essde.optimizator.mixin;
 
 import com.essde.optimizator.OptimizatorScreen;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.Drawable;
+import net.minecraft.client.gui.Element;
+import net.minecraft.client.gui.Selectable;
 import net.minecraft.client.gui.screen.GameMenuScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GameMenuScreen.class)
 public abstract class GameMenuScreenMixin {
+    @Shadow
+    protected abstract <T extends Element & Drawable & Selectable> T addDrawableChild(T drawable);
+
     @Inject(method = "init", at = @At("TAIL"))
     private void optimizator$addButton(CallbackInfo callbackInfo) {
-        GameMenuScreen screen = (GameMenuScreen) (Object) this;
         MinecraftClient client = MinecraftClient.getInstance();
 
         int width = 200;
         int x = (client.getWindow().getScaledWidth() - width) / 2;
-        int y = Math.min(client.getWindow().getScaledHeight() - 55,
-                client.getWindow().getScaledHeight() / 4 + 220);
+        int y = Math.min(
+                client.getWindow().getScaledHeight() - 55,
+                client.getWindow().getScaledHeight() / 4 + 220
+        );
 
-        screen.addDrawableChild(ButtonWidget.builder(
+        this.addDrawableChild(ButtonWidget.builder(
                         Text.literal("Optimizator"),
-                        button -> client.setScreen(new OptimizatorScreen(screen)))
+                        button -> client.setScreen(
+                                new OptimizatorScreen((GameMenuScreen) (Object) this)))
                 .dimensions(x, y, width, 20)
                 .build());
     }
