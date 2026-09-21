e67d09caf61cbaa192f8e5b98930e0d192490b65
To4noHe_BadBoy <essde342@gmail.com>
2026-09-21 12:51:28 +0300

    feat: profile actual particle insertions

--- src/main/java/com/essde/optimizator/mixin/ParticleManagerProfilerMixin.java
+++ src/main/java/com/essde/optimizator/mixin/ParticleManagerProfilerMixin.java
@@ -2,6 +2,8 @@ package com.essde.optimizator.mixin;
 
 import com.essde.optimizator.OptimizatorConfig;
 import com.essde.optimizator.PerformanceProfiler;
+import com.essde.optimizator.PerformanceProfiler;
+import net.minecraft.client.particle.Particle;
 import net.minecraft.client.particle.ParticleManager;
 import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.VertexConsumerProvider;
@@ -12,6 +14,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ParticleManager.class)
 public abstract class ParticleManagerProfilerMixin {
+    @Inject(method = "addParticle(Lnet/minecraft/client/particle/Particle;)V", at = @At("HEAD"))
+    private void optimizator$countParticle(Particle particle, CallbackInfo callbackInfo) {
+        if (OptimizatorConfig.profilerEnabled && particle != null) {
+            PerformanceProfiler.recordParticleCreated();
+        }
+    }
+
     @Inject(method = "renderParticles", at = @At("HEAD"))
     private void optimizator$beginParticles(
             Camera camera,
